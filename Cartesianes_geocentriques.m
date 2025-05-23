fd63bf26dd5b429489ffc6b199280d565fc84df3
Toni <Toni@10.192.90.128>
2025-05-23 15:57:15 +0200

    g

---
+++ Cartesianes_geocentriques.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Cartesianes_(inputArg1,inputArg2)
+%CARTESIANES_ Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
